ceb5d38b865acea3e3e5dddac934698969bdef55
Nick Rafael Salas Diaz <nickrafaeldz@gmail.com>
2025-02-10 12:57:04 -0500

    g

---
+++ script.js
@@ -0,0 +1,39 @@
+function cajaVerde(event) {
+    let container = event.target.closest('.cards-container');
+
+    // Comparar usando el valor en RGB
+    if (container.style.backgroundColor === "rgb(102, 187, 106)") { 
+        container.style.backgroundColor = "#4CAF50"; 
+    } else {
+        container.style.backgroundColor = "#66BB6A"; 
+    }
+}
+
+function cajaAzul(event) {
+    let container = event.target.closest('.cards-container');
+    // Comparar usando el valor en RGB
+    if (container.style.backgroundColor === "rgb(102, 187, 106)") { 
+        container.style.backgroundColor = "#4CAF50"; 
+    } else {
+        container.style.backgroundColor = "#66BB6A"; 
+    }
+}
+function cajaAmarillo(event) {
+    let container = event.target.closest('.cards-container');
+    // Comparar usando el valor en RGB
+    if (container.style.backgroundColor === "rgb(102, 187, 106)") { 
+        container.style.backgroundColor = "#4CAF50"; 
+    } else {
+        container.style.backgroundColor = "#66BB6A"; 
+    }
+}
+
+function cajaRojo(event) {
+    let container = event.target.closest('.cards-container');
+    // Comparar usando el valor en RGB
+    if (container.style.backgroundColor === "rgb(102, 187, 106)") { 
+        container.style.backgroundColor = "#4CAF50"; 
+    } else {
+        container.style.backgroundColor = "#66BB6A"; 
+    }
+}
